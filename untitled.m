2eaf2862508dced9b4c30666c74d23da6d093ca2
Cristian Chitiva <cychitivav@@unal.edu.co>
2022-06-15 23:40:50 -0500

    feat: add W profile to the plot

--- untitled.m
+++ untitled.m
@@ -3,69 +3,113 @@ clc, clear, close all
 L1 = 0.3;
 L2 = 0.2;
 
+Vc = 0.1;
+
 [x0, y0] = forwardKinematics(pi/2,-19*pi/20);
-x0 =0.0313;
-y0 =0.1025;
 q0 = inverseKinematics(x0,y0);
-
-
 
 L = 0.221;
 xc = 0.1605;
 yc = 0.3;
 
-
-s=0:0.05:2*pi;
+s = 0:0.05:2*pi;
 [xf,yf] = path(s);
-qf = inverseKinematics(xf,yf);
-
 xi=xf(1);
 yi=yf(1);
+qf = inverseKinematics(xf,yf);
 
-qi = inverseKinematics(xi,yi);
+Wi = pi;
 
-qs = interp1([0,1],[q0;qi],(0:0.05:1));
+ta = 1;
+tc = 2;
+td = 1;
 
+Wc = (qf(1,:)-q0-1/2*Wi*td)/(1/2*ta+tc+1/2*td); 
 
-[x,y] = inverseKinematics(qf);
+qs = interp1([0,1],[q0;qf(1,:)],(0:0.05:1));
 
-vx = gradient(xf);
-vy = gradient(yf);
-
+% qs = 
+% 
+% 
+% 
 qs = [qs;qf];
-%% Plots
-for i=1:length(qs(:,1))
-    clf
-    rectangle('Position',[xc-L/2 yc-L/2 L L])
-    hold on
-    grid on
-    axis equal
-    
-    
-    yline(yc,'r--')
-    xline(xc-L/2,'r--')
-    axis([-0.01 0.5 -0.01 0.5])
-    xm = L1*cos(qs(i,1));
-    ym = L1*sin(qs(i,1));
+% 
+% 
+% Dqs=gradient(qs);
+% 
+% 
+% q
 
-    [xa,ya] = forwardKinematics(qs(i,1),qs(i,2));
 
-    plot([0 xm],[0 ym],'r')
-    plot([xm xa],[ym ya],'b')
-    plot(xf,yf)
 
-    plot(xi,yi,'o')
 
-    pause(0.001)
-    hold off
+
+[x,y] = forwardKinematics(qs);
+
+Dx = gradient(x);
+Dy = gradient(y);
+
+vx = zeros(1,length(x));
+vy = zeros(1,length(x));
+
+
+t = zeros(1,length(x));
+for i=2:length(x)
+    dx = x(i) - x(i-1);
+    dy = y(i) - y(i-1);
+
+    dt = sqrt(dx^2+dy^2)/Vc;
+
+    vx(i) = dx/dt;
+    vy(i) = dy/dt;
+
+    t(i) = t(i-1)+ dt;
 end
+%% Plots
+% for i=1:10:length(qs(:,1))-1
+%     clf
+%     rectangle('Position',[xc-L/2 yc-L/2 L L])
+%     hold on
+%     grid on
+%     axis equal
+%     
+%     
+%     yline(yc,'r--')
+%     xline(xc-L/2,'r--')
+%     axis([-0.01 0.5 -0.01 0.5])
+%     xm = L1*cos(qs(i,1));
+%     ym = L1*sin(qs(i,1));
+% 
+%     [xa,ya] = forwardKinematics(qs(i,1),qs(i,2));
+% 
+%     plot([0 xm],[0 ym],'r')
+%     plot([xm xa],[ym ya],'b')
+%     plot(x,y)
+% 
+%     plot(xi,yi,'o')
+% 
+% %     legend(strcat('t=',num2str(t(i))))
+% 
+%     pause(t(i+1))
+%     hold off
+% end
 
 
 figure
+subplot(2,1,1)
 hold on
-plot(sqrt(vx.^2+vy.^2))
+plot(t,sqrt(vx.^2+vy.^2))
 grid on
 title('Velocity tip')
+
+
+subplot(2,1,2)
+hold on
+plot(t,vx)
+plot(t,vy)
+grid on
+title('Velocity tip')
+legend('x','y')
 
 figure()
 hold on
